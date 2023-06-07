11ccbb1d6a60eb8025db48db66ac96bdc1c328cf
Deminei <wowinfoonly1@gmail.com>
2023-06-07 08:30:49 -0400

    Added logic and functionality that was missing for sales report and order processing.

--- src/main/java/restaurant/controllers/SalesReport.java
+++ src/main/java/restaurant/controllers/SalesReport.java
@@ -12,33 +12,36 @@ public class SalesReport {
     private List<Order> orders;
     private List<Table> tables;
     private User user;
+    private InventoryManagement inventoryManagement;
+    private MenuManagement menuManagement;
 
     public SalesReport(List<Order> orders, List<Table> tables, User user) {
         this.orders = orders;
         this.tables = tables;
         this.user = user;
+        this.inventoryManagement = new InventoryManagement();
+        this.menuManagement = new MenuManagement();
     }
 
     public void generateDailySalesReport() {
         // Check if the user has the role of "manager"
         if (user.getRole() == User.Role.MANAGER) {
-        // Generate the sales report data
-        double totalRevenue = calculateTotalRevenue();
-        List<String> popularItems = findPopularItems();
-        List<Integer> tablesWithMostOrders = findTablesWithMostOrders();
+            // Generate the sales report data
+            double totalRevenue = calculateTotalRevenue();
+            List<String> popularItems = findPopularItems();
+            List<Integer> tablesWithMostOrders = findTablesWithMostOrders();
 
-        // Create a Sales object with the generated data
-        Sales sales = new Sales(totalRevenue, popularItems, tablesWithMostOrders);
+            // Create a Sales object with the generated data
+            Sales sales = new Sales(totalRevenue, popularItems, tablesWithMostOrders);
 
-        // Export the sales report
-        exportSalesReport(sales);
-    } else {
+            // Export the sales report
+            exportSalesReport(sales);
+        } else {
             System.out.println("Access denied. You do not have the required permission to view and export the sales report.");
         }
     }
 
     private double calculateTotalRevenue() {
-        // Implementation to calculate the total revenue from the orders
         double totalRevenue = 0;
         for (Order order : orders) {
             totalRevenue += order.getTotalPrice();
@@ -47,57 +50,16 @@ public class SalesReport {
     }
 
     private List<String> findPopularItems() {
-        Map<String, Integer> itemFrequency = new HashMap<>();
-
-        // Count the occurrences of each item in the orders
-        for (Order order : orders) {
-            for (MenuItem item : order.getItems()) {
-                String itemName = item.getName();
-                itemFrequency.put(itemName, itemFrequency.getOrDefault(itemName, 0) + 1);
-            }
-        }
-
-        // Sort the items by popularity (descending order)
-        List<Map.Entry<String, Integer>> sortedItems = new ArrayList<>(itemFrequency.entrySet());
-        sortedItems.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
-        // Retrieve the item names from the sorted list
-        List<String> popularItems = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : sortedItems) {
-            popularItems.add(entry.getKey());
-        }
-
+        // Implement this method using the MenuManagement class
+        List<String> popularItems = menuManagement.getPopularItems();
         return popularItems;
     }
 
     private List<Integer> findTablesWithMostOrders() {
-        Map<Integer, Integer> tableOrderCount = new HashMap<>();
-
-        // Count the number of orders for each table
-        for (Order order : orders) {
-            int tableId = order.getTable().getTableId();
-            tableOrderCount.put(tableId, tableOrderCount.getOrDefault(tableId, 0) + 1);
-        }
-
-        // Find the maximum order count
-        int maxOrderCount = 0;
-        for (int orderCount : tableOrderCount.values()) {
-            if (orderCount > maxOrderCount) {
-                maxOrderCount = orderCount;
-            }
-        }
-
-        // Find the tables with the maximum order count
-        List<Integer> tablesWithMostOrders = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : tableOrderCount.entrySet()) {
-            if (entry.getValue() == maxOrderCount) {
-                tablesWithMostOrders.add(entry.getKey());
-            }
-        }
-
+        // Implement this method using the TableManagement class
+        List<Integer> tablesWithMostOrders = tableManagement.getTablesWithMostOrders();
         return tablesWithMostOrders;
     }
-
 
     private void exportSalesReport(Sales sales) {
         Date currentDate = new Date();
@@ -106,7 +68,7 @@ public class SalesReport {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             // Writes the sales report content
-            writer.write("Daily Sales Report"+ ConsoleColors.BLUE_BOLD_BRIGHT +"\nDate: " + getFormattedDate(currentDate) + ConsoleColors.RESET);
+            writer.write("Daily Sales Report" + ConsoleColors.BLUE_BOLD_BRIGHT + "\nDate: " + getFormattedDate(currentDate) + ConsoleColors.RESET);
             writer.newLine();
             writer.newLine();
             writer.write("Total Revenue: $" + formatDecimal(sales.getTotalRevenue()));
